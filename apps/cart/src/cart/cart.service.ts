adefa48d8ace274d3e38440f1415149fb0e2845a
adnankhayrou <adnanekhayrou3@gmail.com>
2024-02-05 09:38:50 +0100

    fixing mongodb connection

--- apps/cart/src/cart/cart.service.ts
+++ apps/cart/src/cart/cart.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Cart, CartDocument } from './entities/cart.entity';
-import { Product, ProductDocument } from 'src/products/entities/product.entity';
+import { Product, ProductDocument } from '../products/entities/product.entity';
 
 @Injectable()
 export class CartService {
