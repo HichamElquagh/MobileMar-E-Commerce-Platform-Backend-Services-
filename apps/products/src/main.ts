7e7bb7b32d74ad0f3d334518f8fc12339697d7c3
MEZ901 <issammez44@gmail.com>
2024-02-06 11:53:59 +0100

    enable cors origin

--- apps/products/src/main.ts
+++ apps/products/src/main.ts
@@ -5,6 +5,14 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: `http://localhost:5173`,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+  });
+
   app.setGlobalPrefix('api');
   await app.listen(configService.get('HTTP_PORT'));
 }
